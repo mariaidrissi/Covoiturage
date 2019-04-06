51719a070b7aef974cf12584704c15feca69beac
Youssef El Saadany <youssef.saadany@gmail.com>
2019-04-06 20:11:25 +0200

    Mise en place sur PHP

--- results.php
+++ results.php
@@ -1,7 +1,15 @@
 <?php require("inc/header.php"); ?>
+<?php require("connexionbdd.php");
+$bdd = new Model(); ?>
+
 <h1>Trajet : BF > PG</h1>
 <h2>Date : 05/12/19<br>
 <small>A partir de 08:00</small></h2><br>
+<?php
+if(isset($_POST['depart'])) { 
+$trajets = $bdd->getAllTrajets();
+foreach($trajets as $trajet) {
+?>
 <div class="result mx-auto my-2 p-2 border rounded-lg">
 	<div class="container">
 		<div class="row">
@@ -9,48 +17,16 @@
 				<img src="generic.png" width="80" height="80">
 			</div>
 			<div class="col-7 text-left my-auto">
-				<h4>BF > PG<br>
-				<small class="text-muted">Thomas</small></h4>
-				<h6>1 places restantes</h6>
+				<h4><?php echo $trajet['depart']; ?> -> <?php echo $trajet['arrivee']; ?><br>
+				<small class="text-muted"><?php echo $trajet['chauffeur']; ?></small></h4>
+				<h6><?php echo $trajet['places']; ?> places restantes</h6>
 			</div>
 			<div class="col-auto p-0 m-auto">
-				<h2>10:00</h2>
+				<h2><?php echo $trajet['heure']; ?></h2>
 			</div>
 		</div>
 	</div>
 </div>
-<div class="result mx-auto my-2 p-2 border rounded-lg">
-	<div class="container">
-		<div class="row">
-			<div class="col-auto p-0 my-auto">
-				<img src="generic.png" width="80" height="80">
-			</div>
-			<div class="col-7 text-left my-auto">
-				<h4>BF > PG<br>
-				<small class="text-muted">Hey</small></h4>
-				<h6>2 places restantes</h6>
-			</div>
-			<div class="col-auto p-0 m-auto">
-				<h2>12:15</h2>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="result mx-auto my-2 p-2 border rounded-lg">
-	<div class="container">
-		<div class="row">
-			<div class="col-auto p-0 my-auto">
-				<img src="generic.png" width="80" height="80">
-			</div>
-			<div class="col-7 text-left my-auto">
-				<h4>BF > PG<br>
-				<small class="text-muted">Khona</small></h4>
-				<h6>3 places restantes</h6>
-			</div>
-			<div class="col-auto p-0 m-auto">
-				<h2>16:30</h2>
-			</div>
-		</div>
-	</div>
-</div>
+<?php }}
+?>
 <?php require("inc/footer.php"); ?>
