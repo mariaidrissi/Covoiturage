0c86e1294ab5f3b8dff8b55ca2f515694dd7679a
mariaidrissi <idrissimaria2@gmail.com>
2019-04-06 18:40:09 +0200

    ajout de la fonction ajoutertrajet

--- connexionbdd.php
+++ connexionbdd.php
@@ -3,7 +3,7 @@
 
 	public function __construct() {
 		try {
-			$this->db = new PDO('pgsql:host=localhost;dbname=gova', 'youssef', 'labasedetest');
+			$this->db = new PDO('pgsql:host=localhost:8889;dbname=covoit', 'root', '');
 		} catch(PDOException $e) {
 			die('<p>La connexion à la base de données a échoué. Erreur['.$e->getCode().'] : ' . $e->getMessage().'</p>');
 		}
@@ -12,7 +12,7 @@
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "ça marche gros";
     }
-	
+
 	public function insererUser($id) {
 		$sql = 'INSERT INTO Users (id) VALUES (:id)';
 		$req = $this->db->prepare($sql);
@@ -24,10 +24,24 @@
 		}
 	}
 
-	
+	public function ajouterTrajet($arrivee, $depart, $nb_places, $heure) {
+	  $sql = 'INSERT INTO Trajet (arrivee,depart,nb_places,heure) VALUES (:arrivee,:depart,:nb_places,:heure)';
+	  $req = $this->db->prepare($sql);
+	  $req->bindParam(':arrivee', $arrivee);
+	  $req->bindParam(':depart', $depart);
+	  $req->bindParam(':nb_places', $nb_places);
+	  $req->bindParam(':heure', $heure);
+
+	  if ($req->execute() == TRUE) {
+	    echo "Nouveau trajet ajouté";
+	  } else {
+	    echo "Error: " . $sql . "<br>" . $this->db->error;
+	  }
+	}
 
 }
 $bdd = new Model();
 echo "lol";
 $bdd->insererUser("lol");
+$bdd->ajouterTrajet(BF,PG, 3, 15:30);
 ?>
