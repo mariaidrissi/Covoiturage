b4c345e83f629daf53cc8d064534322379e0d6b8
anismikou <anis.mikou98@gmail.com>
2019-04-06 19:31:56 +0200

    margin boutons acceuil

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php require("inc/header.php"); ?>
-<a href="add.php" class="btn btn-primary btn-lg align-middle" role="button">Proposer un trajet</a><br>
-<a href="search.php" class="btn btn-primary btn-lg align-middle" role="button">Rechercher un trajet</a>
+<a href="add.php" class="btn btn-primary btn-lg align-middle my-3" role="button">Proposer un trajet</a><br>
+<a href="search.php" class="btn btn-primary btn-lg align-middle my-3" role="button">Rechercher un trajet</a>
 <?php require("inc/footer.php"); ?>
